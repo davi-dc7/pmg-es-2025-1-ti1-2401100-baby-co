3275ff9ee4e73f614ce745991d7dc9bdb1da0e8d
Marcelo Esteves <marcelinho.bernardi@gmail.com>
2025-05-12 09:51:15 -0300

    Correção de erro

---
+++ codigo/Marcelo/script.js
@@ -0,0 +1,15 @@
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const dados = {
+        mae: document.getElementById("mae").value,
+        nascimento_mae: document.getElementById("nascimento_mae").value,
+        pai: document.getElementById("pai").value,
+        nascimento_pai: document.getElementById("nascimento_pai").value,
+    };
+
+    
+    localStorage.setItem("cadastroPais", JSON.stringify(dados));
+
+    alert("Cadastro salvo com sucesso!");
+});
